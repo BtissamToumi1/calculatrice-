ed8cc01f34f656ce20e553f5db70bf3028ec7771
btissamtoumi <btissamtoumi5@gmail.com>
2025-01-25 11:59:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// let table=[];
+// let total;
+// function ajouter(a){
+//       table.push(a);
+//       if(table[0]==="-" || table[0]==="+" ||table[0]==="/" || table[0]==="*"){
+//         document.getElementById(id).textContent=0;
+//       }
+//       else{
+//         //  afficher(a);
+//          console.log(a);
+         
+//         for(let i=0; i<table.length ;i++){
+//             let c=parseInt(tab[i]);
+//             if(table[i]==="+"){
+//                 total+=c;
+//             }
+//             if(table[i==="-"]){
+//                 total-=c;
+//             }
+//             if(table[i]==="/"){
+//                 total/=c;
+//             }
+//             if(table[i]==="*"){
+//                 total*=c;
+//             }
+//             if(table[i]==="="){
+//                 afficher(total);
+//             }
+//         }
+//     }
+// }
+
+// function afficher(a){
+//     const v=document.getElementById("id"); 
+//     v.value=a;
+// }
+const v=document.getElementById("id"); 
+console.log(v.textContent);
+    // v.innerText=7;
