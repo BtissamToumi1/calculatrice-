158de45fdea0194c40784984e4bbd7bdbab22fbc
btissamtoumi <btissamtoumi5@gmail.com>
2025-01-25 12:22:29 +0100

    l'ajout de l'option d'affichage dans la partie output du calculatrice

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const  buttonItem=document.querySelectorAll(".button-item");
+const output=document.getElementById("input");
 //boucle forEach
 
 buttonItem.forEach(item =>{ item.addEventListener('click',function(){
     //recurerer le texte du div clique
     const valeur=this.innerText;
     console.log(valeur)
+    output.innerText=valeur;
 });
 });
