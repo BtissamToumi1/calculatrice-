b3890f5faa015582db843db640ae2b9a1bbdc711
btissamtoumi <btissamtoumi5@gmail.com>
2025-01-25 12:15:56 +0100

    l'ajout de function qui recupere le contenu du buttons clique

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
-// let table=[];
-// let total;
-// function ajouter(a){
-//       table.push(a);
-//       if(table[0]==="-" || table[0]==="+" ||table[0]==="/" || table[0]==="*"){
-//         document.getElementById(id).textContent=0;
-//       }
-//       else{
-//         //  afficher(a);
-//          console.log(a);
-         
-//         for(let i=0; i<table.length ;i++){
-//             let c=parseInt(tab[i]);
-//             if(table[i]==="+"){
-//                 total+=c;
-//             }
-//             if(table[i==="-"]){
-//                 total-=c;
-//             }
-//             if(table[i]==="/"){
-//                 total/=c;
-//             }
-//             if(table[i]==="*"){
-//                 total*=c;
-//             }
-//             if(table[i]==="="){
-//                 afficher(total);
-//             }
-//         }
-//     }
-// }
+const  buttonItem=document.querySelectorAll(".button-item");
+//boucle forEach
 
-// function afficher(a){
-//     const v=document.getElementById("id"); 
-//     v.value=a;
-// }
-const v=document.getElementById("id"); 
-console.log(v.textContent);
-    // v.innerText=7;
+buttonItem.forEach(item =>{ item.addEventListener('click',function(){
+    //recurerer le texte du div clique
+    const valeur=this.innerText;
+    console.log(valeur)
+});
+});
