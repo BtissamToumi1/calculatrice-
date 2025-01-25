29e084a3025ccaa51c783f2e5866e8a5a26dbc3b
btissamtoumi <btissamtoumi5@gmail.com>
2025-01-25 13:16:50 +0100

    l'ajout de fonction ajouter qui permet de stocker les valeurs cliques dans un tableau

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+//les variables
 const  buttonItem=document.querySelectorAll(".button-item");
 const output=document.getElementById("input");
 const clear=document.getElementById("clear");
 
+//pour stocker les valeurs cliques par l'utilisateur
+let table=[];
+let valeur;
+let positionPlus;
+let positionMois;
+let positionDivision;
+let positionSoustraction
 //boucle forEach pout recuperer la valeur clique
 buttonItem.forEach(item =>{ item.addEventListener('click',function(){
     //recurerer le texte du div clique
-    const valeur=this.innerText;
+    valeur=this.innerText;
     console.log(valeur)
+    ajouter()
     output.innerText=valeur;
 });
 });
 
 //pour supprimer les valeurs de la partie output
-
 clear.addEventListener("click",function(){
     output.innerText="";
-})
+    table=[];
+    console.log(table)
+});
+
+//pour ajouter une valeur dans le tableau table
+function ajouter(){
+    table.push(valeur);
+    console.log(table);
+    
+}
+
+
+
