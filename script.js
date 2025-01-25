af4c6e193174fec581d75f1333ae900a5108f46c
btissamtoumi <btissamtoumi5@gmail.com>
2025-01-25 12:29:31 +0100

    l,ajout de fonction clear

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const  buttonItem=document.querySelectorAll(".button-item");
 const output=document.getElementById("input");
-//boucle forEach
+const clear=document.getElementById("clear");
 
+//boucle forEach pout recuperer la valeur clique
 buttonItem.forEach(item =>{ item.addEventListener('click',function(){
     //recurerer le texte du div clique
     const valeur=this.innerText;
@@ -9,3 +10,9 @@ buttonItem.forEach(item =>{ item.addEventListener('click',function(){
     output.innerText=valeur;
 });
 });
+
+//pour supprimer les valeurs de la partie output
+
+clear.addEventListener("click",function(){
+    output.innerText="";
+})
